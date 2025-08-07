bf8b7c4e58a402d1111bd4f0ff8d178537b49da2
estevam02009 <manoel.neto53861@alunos.ufersa.edu.br>
2025-08-07 08:53:39 -0300

    Inciando Projeto

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Restaurant;
+
+class RestaurantController extends Controller
+{
+    public function index()
+    {
+        $restaurants = Restaurant::all();
+        return response()->json($restaurants);
+    }
+}
